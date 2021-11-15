c6ccc38815fe904b5759444eb378c393360fba06
Necrokillerr <samuel.pschr@eduge.ch>
2021-11-15 10:25:21 +0100

    Update grid

--- WF_Picross/WF_Picross/Puzzle.cs
+++ WF_Picross/WF_Picross/Puzzle.cs
@@ -8,13 +8,13 @@ namespace WF_Picross
 {
 	class Puzzle : PictureBox
 	{
-		Cell picross;
+		Cell cell;
 		int _width;
 		int _height;
 
 		public Puzzle() : base()
 		{
-			picross = new Cell();
+			cell = new Cell();
 			_width = 5;
 			_height = 5;
 		}
@@ -66,12 +66,14 @@ namespace WF_Picross
 			for (int i = 0; i < _height; i++)
 			{
 				pe.Graphics.DrawString("0", new Font(FontFamily.GenericSansSerif, 16, FontStyle.Bold), Brushes.Black, new PointF(0, y+32));
+				pe.Graphics.DrawRectangle(Pens.Blue, new Rectangle(new Point(0, y+size), new Size(60, 30)));
 
 				y += size;
 				x = 60;
 				for(int j = 0; j < _width; j++)
 				{
 					pe.Graphics.DrawString("0", new Font(FontFamily.GenericSansSerif, 16, FontStyle.Bold), Brushes.Black, new PointF(x, 0), new StringFormat(StringFormatFlags.DirectionVertical));
+					pe.Graphics.DrawRectangle(Pens.Blue, new Rectangle(new Point(x, 0), new Size(30, 50)));
 					pe.Graphics.DrawRectangle(Pens.Black, new Rectangle(new Point(x, y), new Size(size, size)));
 					x += size;
 				}
