60298f54ad9bc2c3f4e043a5dec21db6b6b311f3
Necrokillerr <samuel.pschr@eduge.ch>
2021-11-29 11:13:31 +0100

    Update Solver class | method fill/cross full rows/collumns

--- WF_Picross/WF_Picross/Puzzle.cs
+++ WF_Picross/WF_Picross/Puzzle.cs
@@ -9,9 +9,14 @@ namespace WF_Picross
 	class Puzzle : PictureBox
 	{
 		Size sizeGrid;
-		public List<Cell> cells;
-		public string[] collumns = { "3", "2,1", "4", "2, 2", "0" };
-		public string[] rows = { "1", "1", "1", "3", "0" };
+		public Cell[,] cells;
+		public List<Cell> cellList;
+
+		public string[] collumns = { "1", "4", "5", "2", "2" };
+		public string[] rows = { "2", "3,1", "4", "3", "1" };
+
+		//public string[] collumns = { "5", "2,1", "4", "2, 2", "0" };
+		//public string[] rows = { "1", "2", "1", "4", "1" };
 
 		public Puzzle() : this(5, 5)
 		{
@@ -26,44 +31,8 @@ namespace WF_Picross
 		public Puzzle(Size size)
 		{
 			sizeGrid = size;
-			cells = new List<Cell>();
-		}
-
-		// ================ Méthodes ================
-		// Met une croix
-		public void PutX()
-		{
-
-		}
-
-		// Remplis la case
-		public void PutColor()
-		{
-
-		}
-
-		// Met une croix aux deux extrémités d'un segment
-		public void PutXAround()
-		{
-			// Si nbCase à remplir = nbCase rempli, Mettre une croix aux extrémités
-		}
-
-		// Met une croix dans les espaces trop petit pour le nombre
-		public void PutXInLittleSpaceForHint()
-		{
-			// Si nbCase disponible < que nbCase à remplir, Remplir par des croix
-		}
-
-		// Remplis toutes les cellules correct "logique"
-		public void FillBetweenCell()
-		{
-			// Si deux case sont remplie et qu'elles correspondent au même nombre, Remplir les cases du milieu
-		}
-
-		// Remplis les lignes ou colonnes complète
-		public void FillFullLineOrColumnCell()
-		{
-			// Si nbCase disponible = nbCase à remplir (dans ligne ou colonne), Tout remplir
+			cells = new Cell[sizeGrid.Width, sizeGrid.Height];
+			cellList = new List<Cell>();
 		}
 
 		protected override void OnPaint(PaintEventArgs pe)
@@ -84,14 +53,20 @@ namespace WF_Picross
 				{
 					pe.Graphics.DrawString(collumns[j], new Font(FontFamily.GenericSansSerif, 16, FontStyle.Bold), Brushes.Black, new PointF(x, 0), new StringFormat(StringFormatFlags.DirectionVertical));
 					pe.Graphics.DrawRectangle(Pens.Blue, new Rectangle(new Point(x, 0), new Size(30, 50)));
-					cells.Add(new Cell(new Point(x, y), new Size(size, size)));
+					cells[i, j] = new Cell(new Point(x, y), new Size(size, size));
+					cellList.Add(new Cell(new Point(x, y), new Size(size, size)));
 					x += size;
 				}
 			}
-			foreach (Cell cell in cells)
+			/*foreach (Cell cell in cells)
             {
 				cell.Paint(pe.Graphics);
-            }
+            }*/
+
+			foreach (Cell cell in cellList)
+			{
+				cell.Paint(pe.Graphics);
+			}
 		}
 	}
 }
--- WF_Picross/WF_Picross/Solver.cs
+++ WF_Picross/WF_Picross/Solver.cs
@@ -14,16 +14,71 @@ namespace WF_Picross
 
         public void Solve()
         {
-            for(int i = 0; i < pCross.collumns.Length; i++)
+            //PutCrossOnRowsAndCollumnsWithZero();
+            //PutColorOnRowsAndCollumnsWithMaxValue();
+            PutColorOnCenteredCells();
+        }
+
+        // Met une croix dans les lignes et colonnes ayant la valeur minimal (zéro)
+        public void PutCrossOnRowsAndCollumnsWithZero()
+        {
+            // Collumns
+            for (int i = 0; i < pCross.collumns.Length; i++)
             {
                 if (pCross.collumns[i] == "0")
                 {
                     for (int j = 0; j < pCross.rows.Length; j++)
                     {
-                        pCross.cells[j].state = State.cross;
+                        pCross.cellList[i + pCross.rows.Length * j].state = State.cross;
                     }
                 }
             }
+            
+            // Rows
+            for (int i = 0; i < pCross.rows.Length; i++)
+            {
+                if (pCross.rows[i] == "0")
+                {
+                    for (int j = 0; j < pCross.collumns.Length; j++)
+                    {
+                        pCross.cellList[j + pCross.collumns.Length * i].state = State.cross;
+                    }
+                }
+            }
+        }
+
+        // Remplis toutes les lignes et colonnes ayant la valeur maximal (taille du puzzle)
+        public void PutColorOnRowsAndCollumnsWithMaxValue()
+        {
+            // Collumns
+            for (int i = 0; i < pCross.collumns.Length; i++)
+            {
+                if (pCross.collumns[i] == pCross.collumns.Length.ToString())
+                {
+                    for (int j = 0; j < pCross.rows.Length; j++)
+                    {
+                        pCross.cellList[i + pCross.rows.Length * j].state = State.fill;
+                    }
+                }
+            }
+
+            // Rows
+            for (int i = 0; i < pCross.rows.Length; i++)
+            {
+                if (pCross.rows[i] == pCross.rows.Length.ToString())
+                {
+                    for (int j = 0; j < pCross.collumns.Length; j++)
+                    {
+                        pCross.cellList[j + pCross.collumns.Length * i].state = State.fill;
+                    }
+                }
+            }
+        }
+
+        // Remplis les cases restant allumé quand la valeur à été testé dans chaque emplacement
+        public void PutColorOnCenteredCells()
+        {
+
         }
     }
 }
